4bc9d8dcf9dccda414d9db19ec6be749c85a7588
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-12-09 09:16:31 -0300

    update

--- aliases.zsh
+++ aliases.zsh
@@ -15,7 +15,7 @@ alias trash="send2trash"
 
 alias open="xdg-open"
 alias tl="tldr --list | fzf --preview 'tldr {} --color always' | xargs tldr"
-alias hypr="vim .config/hypr/hyprland.conf.d/"
+alias hypr="vim ~/.config/hypr/hyprland.conf.d/"
 alias mon="vim ~/.config/hypr/hyprland.conf.d/11-monitors.conf"
 alias monitors="vim ~/.config/hypr/hyprland.conf.d/11-monitors.conf"
 alias mond='hyprctl keyword monitor "eDP-1, disable"'
