15d3cd63b323904a7e2bf16f6b356a3fe0e587e6
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-02-10 13:32:13 -0300

    update

--- config/waybar/scripts/mediaplayer.py
+++ config/waybar/scripts/mediaplayer.py
@@ -65,6 +65,8 @@ class PlayerManager:
         return self.manager.props.players
 
     def write_output(self, text, player):
+        if len(text) > 30:
+            text = text[:30] + "..."
         logger.debug(f"Writing output: {text}")
 
         output = {"text": text,
@@ -123,12 +125,13 @@ class PlayerManager:
             track_info = title
 
         if track_info:
-            if player.props.status == "Playing":
+            if player.props.status != "Playing":
                 track_info = " " + track_info
             else:
                 track_info = " " + track_info
         # only print output if no other player is playing
         current_playing = self.get_first_playing_player()
+
         if current_playing is None or current_playing.props.player_name == player.props.player_name:
             self.write_output(track_info, player)
         else:
